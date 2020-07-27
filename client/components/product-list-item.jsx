19a2c51a972b2c3ee3c58ec5c0473247495919a6
tim ahn <tim.ahn3@gmail.com>
2020-07-27 14:35:28 -0700

    wip

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,14 +4,20 @@ function ProductListItem(props) {
   return (
     <div className="col-md-4">
       <div className="card card-width">
-        <img src={props.image} className="card-img-top contain" alt=""></img>
-        <div className="card-body text-container">
+        <div className="image-container">
+          <img src={props.image} className="card-img-top img-dimensions" alt=""></img>
+        </div>
+
+        <div className="card-body">
           <h5 className="card-title">{props.name}</h5>
           <h6 className="card-subtitle">{props.price}</h6>
           <p className="card-text">{props.shortDescription}</p>
         </div>
+
       </div>
+
     </div>
+
   );
 }
 export default ProductListItem;
