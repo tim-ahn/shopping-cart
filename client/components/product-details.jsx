8b38cf0e16f5f760add5a874fbd3079db282f371
tim ahn <tim.ahn3@gmail.com>
2020-08-17 16:52:04 -0700

    created ProductDetails component with initial state property named product set to null and render method that returns null

---
+++ client/components/product-details.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
+export default ProductDetails;
